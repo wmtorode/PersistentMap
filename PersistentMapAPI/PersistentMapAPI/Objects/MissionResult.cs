6302636a8fc6a050890567790ad7b04bbe7cda99
Bill Torode <wmtorode@gmail.com>
2020-05-21 21:54:24 -0400

    fix serialization issue with API

--- PersistentMapAPI/PersistentMapAPI/Objects/MissionResult.cs
+++ PersistentMapAPI/PersistentMapAPI/Objects/MissionResult.cs
@@ -2,8 +2,8 @@
 
 namespace PersistentMapAPI {
     public class MissionResult {
-        public FactionValue employer;
-        public FactionValue target;
+        public string employer;
+        public string target;
         public BattleTech.MissionResult result;
         public string systemName;
         public int difficulty;
@@ -15,10 +15,10 @@ namespace PersistentMapAPI {
         public MissionResult(FactionValue employer, FactionValue target, BattleTech.MissionResult result, string systemName, int difficulty, int awardedRep, int planetSupport, int mCount, string missionType) {
             this.awardedRep = awardedRep;
             this.difficulty = difficulty;
-            this.employer = employer;
+            this.employer = employer.Name;
             this.result = result;
             this.systemName = systemName;
-            this.target = target;
+            this.target = target.Name;
             this.planetSupport = planetSupport;
             this.mCount = mCount;
             this.missionType = missionType;
